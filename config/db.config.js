74890a52ae1080d66c5ca2576814a4b34a2e829c
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2019-04-03 07:49:50 -0700

    Added settings

--- config/db.config.js
+++ config/db.config.js
@@ -6,14 +6,14 @@ dotenv.config();
 
 let sequelize;
 
-if (process.env.DATABASE_URL) {
+if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
 
   /** @type {Sequelize} [database connection for heroku production] */
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
+  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
-    port:     5432,
-    host:     "<heroku host>",
+    port:     match[4],
+    host:     match[3],
     logging:  true //false
   });
 
