c8ef2e931d69460ddd6ed1dc5ddd99b2bbbabc1e
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2019-04-04 03:19:18 -0700

    Disabled cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 /** set up middlewares */
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
